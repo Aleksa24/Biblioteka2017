a75da0b1b42ca1f368402a71eaa7e6509d1c3969
alkesa24444 <aleksaivanovic24444@gmal.com>
2017-03-14 15:15:27 +0100

    kiuk

--- Biblioteka2017/src/biblioteka/Knjiga.java
+++ Biblioteka2017/src/biblioteka/Knjiga.java
@@ -2,6 +2,44 @@ package biblioteka;
 
 public class Knjiga {
 
+	private String naslov;
+	private long isbn;
+	private String izdavac;
+	private int izdanje;
+	private String autor;
+	
+	
+	public String getNaslov() {
+		return naslov;
+	}
+	public void setNaslov(String naslov) {
+		this.naslov = naslov;
+	}
+	public long getIsb() {
+		return isbn;
+	}
+	public void setIsb(long isbn) {
+		this.isbn = isbn;
+	}
+	public String getIzdavac() {
+		return izdavac;
+	}
+	public void setIzdavac(String izdavac) {
+		this.izdavac = izdavac;
+	}
+	public int getIzdanje() {
+		return izdanje;
+	}
+	public void setIzdanje(int izdanje) {
+		this.izdanje = izdanje;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	
 	
 	
 }
